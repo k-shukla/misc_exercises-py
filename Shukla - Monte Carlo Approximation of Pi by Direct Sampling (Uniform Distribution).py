30664fa9ed6db6906e4c5d518a08cae108a7078b
Karpur Shukla <k-shukla@users.noreply.github.com>
2017-07-05 14:46:50 -0400

    Update Shukla - Monte Carlo Approximation of Pi by Direct Sampling (Uniform Distribution).py

--- Shukla - Monte Carlo Approximation of Pi by Direct Sampling (Uniform Distribution).py
+++ Shukla - Monte Carlo Approximation of Pi by Direct Sampling (Uniform Distribution).py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 
-'''Here, we use a direct-sampling Monte Carlo simulation to calculate the value of π. 
+''' Here, we use a direct-sampling Monte Carlo simulation to calculate the value of π. 
+    
+    The conceptual model we have is of performing a number of uniformly-distributed trials inside a
+    square, whose length we normalise to be 1. We imagine a circle of diameter one cocentric with the
+    square, and we perform a number of trials (i.e. generate a number of points) inside the square.
+    
+    Since we know the ratio of the areas between the circle and the square is A_cir / A_sqr = π/4,
+    and since the trials are uniformly distributed inside the square, we immediately have
+    N_cir / N_sqr = π/4; reversing this gives the value of π as π = 4 N_cir / N_sqr.
+    (Note that N_sqr is the number of trials: we don't count anything outside of the square.)'''
    
-   The conceptual model we have is of performing a number of uniformly-distributed trials inside a
-   square, whose length we normalise to be 1. We imagine a circle of diameter one cocentric with the
-   square, and we perform a number of trials (i.e. generate a number of points) inside the square.
-   
-   Since we know the ratio of the areas between the circle and the square is A_cir / A_sqr = π/4,
-   and since the trials are uniformly distributed inside the square, we immediately have
-   N_cir / N_sqr = π/4; reversing this gives the value of π as π = 4 N_cir / N_sqr.
-   (Note that N_sqr is the number of trials: we don't count anything outside of the square.)'''
-   
-'''For this, we generate a number of (x, y) points in the range [-1, 1), which represents points in
-   the square. To test whether they're inside the circle as well, we want to check x^2 + y^2; if
-   x^2 + y^2 < 1, then the point is inside the circle. (This is why I'm generating points inside
-   [-1, 1) rather than [0, 1): centering at the origin lets me check whether a point is inside the
-   circle way more simply.)
-   
-   Since I only need to generate random points and check them, I don't actually need to store them
-   anywhere.'''
+''' For this, we generate a number of (x, y) points in the range [-1, 1), which represents points in
+    the square. To test whether they're inside the circle as well, we want to check x^2 + y^2; if
+    x^2 + y^2 < 1, then the point is inside the circle. (This is why I'm generating points inside
+    [-1, 1) rather than [0, 1): centering at the origin lets me check whether a point is inside the
+    circle way more simply.)
+    
+    Since I only need to generate random points and check them, I don't actually need to store them
+    anywhere. '''
 
 
 # This section imports the libraries necessary to run the program.
@@ -33,10 +33,10 @@ import time
 # This section stores the time at the start of the program.
 program_start_time = time.clock()
 
-'''Since we're interested in the amount of time the program will run in, we'll store the time at the
-   beginning of the program using time.clock(), and compare it to the time at the end (again using
-   time.clock(), applied to a different variable name. time.clock() just takes the time at a given
-   moment; it's up to us to store it properly.'''
+''' Since we're interested in the amount of time the program will run in, we'll store the time at the
+    beginning of the program using time.clock(), and compare it to the time at the end (again using
+    time.clock(), applied to a different variable name. time.clock() just takes the time at a given
+    moment; it's up to us to store it properly. '''
 
 
 # This section sets the number of Monte Carlo trials, as well as the number of times a single set of trials is run.
